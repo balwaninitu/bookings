a23cc9d8f97cea3786a42e00ec0c6ef1b93493dd
Nitu Balwani <balwaninitu@gmail.com>
2021-06-23 14:27:13 +0800

    html templates added 3rd commit

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -59,3 +59,15 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	})
 
 }
+
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.html", &models.TemplateData{})
+}
